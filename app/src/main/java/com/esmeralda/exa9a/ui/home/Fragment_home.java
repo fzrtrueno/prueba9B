d92974a04202dc646180cd681baa2d4b77c806a7
Francisco Zavala <fzavala@utcam.edu.mx>
2023-06-12 11:25:08 -0600

    proyecto del examen

---
+++ app/src/main/java/com/esmeralda/exa9a/ui/home/Fragment_home.java
@@ -0,0 +1,6 @@
+package com.esmeralda.exa9a.ui.home;
+
+import android.app.Activity;
+
+public class Fragment_home extends Activity {
+}
---
+++ app/src/main/java/com/esmeralda/exa9a/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.esmeralda.exa9a.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
